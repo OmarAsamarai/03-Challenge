360aff336e887adb377804ccbad54c4c08a074db
OmarAsamarai <oasamarai@gmail.com>
2021-12-12 23:30:41 -0600

    kind of final

--- script.js
+++ script.js
@@ -1,66 +1,59 @@
 // Assignment Code
+ 
 var generateBtn = document.querySelector("#generate");
-var numbersEl = document.getElementById('numbers');
-var uppercaseEl = document.getElementById('uppercase');
-var lowercaseEl = document.getElementById('lowercase');
-var symbolEl = document.getElementById('symbol');
-var getRandomSymbol = document.getElementById('randomsymbol')
-var getRandomLower = document.getElementById('randomlower')
-var getRandomUpper = document.getElementById('randomupper')
-var getRandomNumber = document.getElementById('randomnumber')
+var submit;
+var number;
+var symble;
+var uppercase;
+var lowercase;
+var proceed;
+var charLength;
+var psLength;
+var keyToAdd;
+var symbol;
+var charElemnt;
 
-var randomFunc = {
-  lower: getRandomLower,
-  number: getRandomNumber,
-	upper: getRandomUpper,
-	symbol: getRandomSymbol,
-}
-
-
-// const resultEl = document.getElementById('result');
-// const lengthEl = document.getElementById('length');
-// const uppercaseEl = document.getElementById('uppercase');
-// const lowercaseEl = document.getElementById('lowercase');
-// const numbersEl = document.getElementById('numbers');
-// const symbolsEl = document.getElementById('symbols');
-// const generateEl = document.getElementById('generate');
-// const clipboard = document.getElementById('clipboard');
-
+var psLength = document.getElementById("legth");
+var numberConfirm = document.getElementById("number");
+var upperConfirm = document.getElementById("uppercase");
+var loweConfirm = document.getElementById("lowercase");
+var symboConfirm = document.getElementById("symbols");
 
 function generatePassword() {
 
   // Password length -------------------------------------
-var lengthEl = function() {
-  var length = "";
-  
-  if (length < 8 || length >128) {
-    length = prompt("Choose password length between 8 and 128 charactors");
+
+var charLength = prompt("How many characters will your password be? Enter a number between 8 and 128");
+if (!charLength) {
+  alert("Please write a value");
+}
+if (charLength < 8 || charLength >128) {
+  var charLength = prompt("Please choose character amount between 8 and 128");
+  }
+  else {
+  console.log(charLength)
   }
   
-  return length;
-
-};
-
-console.log(length);
-var lengthEl = {
-  length: lengthEl(),
-}
-
 
 
 // Choose the numbers -----------------------------------
 var numberConfirm = window.confirm("Do you want to use numbers in the password?");
 if (numberConfirm) {
-  function getRandomNumber() {
-    return (Math.floor(Math.random() * 10) + 48);  
+  function numberConfirm() {
+    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
   }
   
-    
+  number = numberConfirm();  
+  console.log(number);
   //proceed
 } else {
   proceed;
 }
-window.alert(getRandomNumber());
+
+
+
+
+
 
 
 // Choose the characters -----------------------------------
@@ -72,13 +65,14 @@ if (symbolConfirm) {
 
     
   }
-  
+  symbol = getRandomSymbol();
+console.log(symbol);
     
   //proceed
 } else {
   proceed;
 }
-console.log(getRandomSymbol());
+
 
 
 // Choose the uppercase -----------------------------------
@@ -88,43 +82,39 @@ if (uppercaseConfirm) {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
   }
   
-  
+  uppercase = getRandomUpper()
+console.log(uppercase);
 
   //proceed
 } else {
   proceed;
 }
-console.log(getRandomUpper());
+
+
 
 // Choose the lowercase ------------------------------------
 var lowercaseConfirm = window.confirm("Do you want to use lowercase in the password?");
-if (uppercaseConfirm) {
+if (lowercaseConfirm) {
   function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
   }
-  
+  lowercase = getRandomLower();
+
+console.log(lowercase);
 
   //proceed
 } else {
   proceed;
 }
-
-console.log(getRandomLower());
-
-
+console.log(number, symbol, uppercase, lowercase)
+if (symbolConfirm+numberConfirm+uppercaseConfirm+lowercaseConfirm === 0) {
+  choices = alert("You must choose a criteria!");
   return
 }
-
-
-
-
-// // function to choose lower case ----------------------------
-// var uppercaseEl = confirm("Do you want to use upppercase in ?");
-// if (proceed) {
-//   //proceed
-// } else {
-//   //don't proceed
-// }
+else {
+  proceed;
+}
+}
 
 
 
@@ -136,7 +126,12 @@ function writePassword() {
 
   passwordText.value = password;
 
+  
 }
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
