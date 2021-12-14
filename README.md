89f28f2596090f259b10a3a95c20859908cc9862
OmarAsamarai <oasamarai@gmail.com>
2021-12-13 18:14:24 -0600

    new update

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Random Password Generator
 ## <li> According to the intructor, the obligation was to prepare a randomlly pasword generator.
 
-## according to tht acceptance criteria I started with the propts to ask the person the assigned questions:
+## according to tht acceptance criteria I started with the prompts to ask the person about the criteria they want in the password.
 ## <li> To choose numbe of characters of the password between 8 and 128.
-## <li> To choose if they want numbers, symbols, uppercase letters, and lowercase letters.
-## Altho I spent a lot of hours trying but unfortunately I did not go further, I need to try more later.
+## <li> To choose if they want numbers, symbols, uppercase letters, and lowercase letters in it.
+## Then, I went to excute the and showing the password in the sapce.
+## If the person chose to a password length < 8 or > 128, the prompt will ask again and if they insist the process will stop.
+## At least one criteria has to be choosen, othrwise, no password will be generated.
 <hr>
 
 ## HTML, CSS, and Java Script were used in this work 
--- script.js
+++ script.js
@@ -1,123 +1,99 @@
-// Assignment Code
+// Definitions ---
  
 var generateBtn = document.querySelector("#generate");
-var submit;
-var number;
-var symble;
-var uppercase;
-var lowercase;
-var proceed;
-var charLength;
-var psLength;
-var keyToAdd;
-var symbol;
-var charElemnt;
-
-var psLength = document.getElementById("legth");
 var numberConfirm = document.getElementById("number");
 var upperConfirm = document.getElementById("uppercase");
 var loweConfirm = document.getElementById("lowercase");
 var symboConfirm = document.getElementById("symbols");
 
+var underArray = [];
+var pwArray = [];  
+
+//function generate password
 function generatePassword() {
 
-  // Password length -------------------------------------
-
+  // Choose the Password length -------------------------------------
 var charLength = prompt("How many characters will your password be? Enter a number between 8 and 128");
 if (!charLength) {
   alert("Please write a value");
+  var charLength = prompt("Please choose character amount between 8 and 128");
 }
 if (charLength < 8 || charLength >128) {
   var charLength = prompt("Please choose character amount between 8 and 128");
+}
+if (charLength < 8 || charLength >128) {
+  return;
   }
-  else {
-  console.log(charLength)
-  }
-  
-
 
 // Choose the numbers -----------------------------------
-var numberConfirm = window.confirm("Do you want to use numbers in the password?");
+var numberConfirm = confirm("Do you want to use numbers in the password?");
 if (numberConfirm) {
   function numberConfirm() {
     return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
   }
-  
   number = numberConfirm();  
   console.log(number);
   //proceed
-} else {
-  proceed;
 }
 
-
-
-
-
-
-
 // Choose the characters -----------------------------------
-var symbolConfirm = window.confirm("Do you want to use the symbols (!@#$%^&*(){}[]=<>/,.) in the password?");
+var symbolConfirm = window.confirm("Do you want to inclue the symbols (!@#$%^&*(){}[]=<>/) in the password?");
 if (symbolConfirm) {
   function getRandomSymbol() {
     var symbols = '!@#$%^&*(){}[]=<>/,.'
-    return symbols[Math.floor(Math.random() * symbols.length)];
-
-    
+    return symbols[Math.floor(Math.random() * symbols.length)];  
   }
   symbol = getRandomSymbol();
 console.log(symbol);
     
   //proceed
-} else {
-  proceed;
-}
-
-
+} 
 
 // Choose the uppercase -----------------------------------
-var uppercaseConfirm = window.confirm("Do you want to use uppercase in the password?");
+var uppercaseConfirm = window.confirm("Do you want to inclue uppercase in the password?");
 if (uppercaseConfirm) {
   function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
   }
-  
   uppercase = getRandomUpper()
-console.log(uppercase);
-
-  //proceed
-} else {
-  proceed;
 }
 
-
-
 // Choose the lowercase ------------------------------------
-var lowercaseConfirm = window.confirm("Do you want to use lowercase in the password?");
+var lowercaseConfirm = window.confirm("Do you want to inclue lowercase in the password?");
 if (lowercaseConfirm) {
   function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
   }
   lowercase = getRandomLower();
 
-console.log(lowercase);
-
-  //proceed
-} else {
-  proceed;
 }
-console.log(number, symbol, uppercase, lowercase)
+// If nothig chosen, no password will be generated.
 if (symbolConfirm+numberConfirm+uppercaseConfirm+lowercaseConfirm === 0) {
-  choices = alert("You must choose a criteria!");
-  return
+  choices = alert("You must choose a criteria!");      
+    }
+
+// Collcting The characters together ------------
+if (numberConfirm){
+  underArray =underArray.concat(number)
 }
-else {
-  proceed;
+if (symbolConfirm){
+  underArray =underArray.concat(symbol)
 }
+if (uppercaseConfirm){
+  underArray =underArray.concat(uppercase)
+}
+if (lowercaseConfirm){
+  underArray =underArray.concat(lowercase)
 }
 
-
-
+for (var i = 0; i < charLength; i++) {
+      
+  pwArray.push (underArray[Math.floor(Math.random() * underArray.length)]); 
+  }
+  return pwArray.join("") ;
+  
+return
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -125,14 +101,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  
+ 
 }
-
-
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
